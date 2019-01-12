b40bfc23e4b7b90b7a31e019f920cef8191043d0
Mike Skowronek <skowronkow@gmail.com>
2019-01-12 19:22:18 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
