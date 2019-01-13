a72e3da485b958b291df3f0c91639d03599c5201
Mike Skowronek <skowronkow@gmail.com>
2019-01-13 18:06:10 +0100

    Add prettier-standard

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom'
 
 const title = 'Hello React UI Interactions'
 
-ReactDOM.render(<div>{title}</div>, document.getElementById('app'))
+ReactDOM.render(
+  <div className='application'>{title}</div>,
+  document.getElementById('app')
+)
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
