0ab4123dc09bce9a6da70bc809c3a72fcf068e49
Mike Skowronek <skowronkow@gmail.com>
2019-01-19 22:21:07 +0100

    Fix missing 'to' props in Link components

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,8 +4,8 @@ import { Link } from '@reach/router'
 export default () => {
   return (
     <nav>
-      <Link>Home</Link>
-      <Link>About</Link>
+      <Link to='/'>Home</Link>
+      <Link to='about'>About</Link>
     </nav>
   )
 }
